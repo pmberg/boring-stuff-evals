a332caa4eac96ba35c870a3b448dd17c2e2bfdeb
pmberg <berggrenpeterm@gmail.com>
2025-08-07 14:56:58 -0400

    Added flexibility to take filepath as a second argument

--- docker-file-test.ps1
+++ docker-file-test.ps1
@@ -1,5 +1,13 @@
+#Sets a parameter, including a default value if the second argument is omitted
+param (
+    [string]$FilePath = "basics/docker-file-test.py"
+)
+
+# Convert relative path to full path (Docker requires absolute)
+$fullPath = Resolve-Path $FilePath
+
 docker run --rm `
   --memory="512m" `
   --network=none `
-  -v "$(pwd)/basics/docker-file-test.py:/code/docker-file-test.py:ro" `
-  python-eval-1 python /code/docker-file-test.py
+  -v "$($fullPath):/code/eval.py:ro" `
+  python-eval-1 python /code/eval.py
